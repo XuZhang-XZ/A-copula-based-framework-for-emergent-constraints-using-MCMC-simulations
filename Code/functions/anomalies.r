4782164b994ec984d3b3159cd74b48d07659002c
XU ZHANG <77970978+870846704@users.noreply.github.com>
2025-01-03 14:55:44 +0800

    Delete Code directory

--- Code/functions/anomalies.r
+++
@@ -1,9 +0,0 @@
-
-anomalies=function(x,monthss){
-  season.mean=0
-  for(i in 1:12){
-    season.mean[i]=mean(x[which(monthss==i)],na.rm=TRUE)
-  }
-  anoma=x-season.mean[monthss]
-  return(anoma)
-}
